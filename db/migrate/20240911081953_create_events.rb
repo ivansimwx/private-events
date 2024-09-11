79a084db8c7238b27156101952339d9aee5578d6
ivansimwx <16796516+ivansimwx@users.noreply.github.com>
2024-09-11 16:28:53 +0800

    Create event model, controller with index view

---
+++ db/migrate/20240911081953_create_events.rb
@@ -0,0 +1,11 @@
+class CreateEvents < ActiveRecord::Migration[7.2]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.string :desc
+      t.date :date
+
+      t.timestamps
+    end
+  end
+end
