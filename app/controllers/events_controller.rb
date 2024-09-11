a8902d21956004351bbc700d8c72acf9181e95a9
ivansimwx <16796516+ivansimwx@users.noreply.github.com>
2024-09-11 16:49:08 +0800

    Add new and create routes for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, only: [ :new, :create ]
+  # before_action :authenticate_user!, only: [ :new, :create ]
 
   # def new
   #   @post = Post.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :events, only: [ :new, :create, :index ]
   root "events#index"
-  get "events/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
